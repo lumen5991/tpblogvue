13465e500484d7768cee242dcbff656ca35b5ccd
lumen <lumenahle@gmail.com>
2023-08-03 23:29:21 +0100

    first commit

---
+++ src/views/Blog.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+import img1 from "@/assets/images/img1.svg"
+import img2 from "@/assets/images/img2.svg"
+import img3 from "@/assets/images/img3.svg"
+import img4 from "@/assets/images/img4.svg"
+import img5 from "@/assets/images/img5.svg"
+import img6 from "@/assets/images/img6.svg"
+import img7 from "@/assets/images/img7.svg"
+import img8 from "@/assets/images/img8.svg"
+    const tableau = ref([
+        {
+            title:"Qu'est-ce que la rédation de contenu ? 9 conseils pour vous aider à publier d'excellents contenus",
+            summary :"La rédaction de contenu est le processus de planification, de rédaction et d‘édition de contenu web, généralement à des fins de marketing numérique. Il peut s‘agir de rédiger des billets de blog et des articles, des scripts pour des vidéos et des podcasts, ainsi que du contenu pour des plateformes spécifiques, comme des tweetstorms sur Twitter ou des posts sur Reddit.",
+            id:1,
+            content:"Qu'est-ce que la rédaction de contenu ? La rédaction de contenu est le processus de planification, de rédaction et d'édition de contenu web, généralement à des fins de marketing numérique. Il peut s'agir de la rédaction de billets de blog et d'articles, de scripts pour des vidéos et des podcasts, ainsi que de contenus destinés à des plateformes spécifiques, comme les tweetstorms sur Twitter ou les messages texte sur Reddit. Pourquoi est-il important de bien rédiger votre contenu ?Lorsque la plupart des gens entendent « rédaction de contenu », ils pensent « rédaction d'articles ».Cependant, la rédaction de contenu n'est pas seulement importante pour les articles de blog.En effet, la rédaction de contenu est essentielle pour tous les types de formats de contenu",
+            image: img1
+        },
+        {
+            title:"Guide des paramètres URL pour les débutants",
+            summary :"Dans ce guide complet, nous explorons les divers aspects des paramètres URL. Découvrez comment utiliser les chaînes de requête sans nuire à vos classements.",
+            id:2,
+            content:"Paramètres URL et leur impact sur le SEO.Les paramètres URL font partie intégrante des structures URL. Bien qu'ils soient un atout inestimable entre les mains de professionnels chevronnés du SEO, les chaînes de requête présentent souvent de sérieux défis pour les classements de votre site web.Dans ce guide, vous trouverez les problèmes de SEO les plus courants auxquels vous devez prêter attention lorsque vous travaillez avec des paramètres URL. Que sont les paramètres URL ? Les paramètres URL (également appelés « chaînes de requête » ou « paramètres de requête d'URL ») sont des éléments insérés dans vos URL pour vous aider à filtrer et à organiser le contenu ou à suivre des informations sur votre site web. En un mot, les paramètres URL sont un moyen de transmettre des informations sur un clic en utilisant l'URL elle-même.Pour identifier un paramètre URL, reportez-vous à la partie de l'URL qui suit le point d'interrogation (?). Les paramètres URL sont composés d'une clé et d'une valeur, séparées par un signe égal (=). Les paramètres multiples sont ensuite séparés par une esperluette (&).",
+            image: img2
+        }, 
+        {
+            title:"Guide du débutant en Content Marketing B2B : les tactiques à connaître pour réussir du premier coup",
+            summary :"Dans cet article, nous allons vous présenter une sélection d‘exemples de FAQ et vous fournir différents modèles pour vous aider à créer une excellente expérience client pour votre audience.",
+            id:3,
+            content:"Les sections de Foire aux questions (FAQ) sont l'occasion d'établir un lien avec votre audience, de la surprendre et la séduire en offrant une très bonne expérience de service clientèle.Les FAQ sont généralement conçues après coup et compilées à la hâte pour répondre à un besoin, ce qui est dommage, car votre FAQ représente une véritable opportunité d'augmenter votre taux de conversion en établissant un lien avec votre audience.Nous allons vous guider à travers une sélection d'exemples de FAQ et vous fournir différents modèles FAQ pour vous aider à créer une excellente expérience client pour votre audience.",
+            image:img3
+        }, 
+        {
+            title:'',
+            summary :'',
+            id:'',
+            content:'',
+            image: img4
+        }, 
+        {
+            title:'',
+            summary :'',
+            id:'',
+            content:'',
+            image:img5
+
+        }, 
+        {
+            title:'',
+            summary :'',
+            id:'',
+            content:'',
+            image: img6
+        }, 
+        {
+            title:'',
+            summary :'',
+            id:'',
+            content:'',
+            image: img7
+        },
+        {
+            title:'',
+            summary :'',
+            id:'',
+            content:'',
+            image: img8
+        }
+    ])
+</script>
+<template>
+    
+
+
+
+
+</template>
